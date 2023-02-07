4e16caff19ff766b9f9e146be3fe1ea102fc57f4
Camilo Carabali <73615948+camiloCarabali@users.noreply.github.com>
2023-02-07 08:42:41 -0500

    hosting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,9 @@
-import * as functions from 'firebase-functions';
-import * as express from 'express';
-import  { addTravel } from './TravelController';
+import * as functions from "firebase-functions";
 
-const app = express();
-
-app.get('/', (req, res) => res.status(200).send('Hey there!'));
-app.post('/travel', addTravel)
-
-exports.app = functions.https.onRequest(app);
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
