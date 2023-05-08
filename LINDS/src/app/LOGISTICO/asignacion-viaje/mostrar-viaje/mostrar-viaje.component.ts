15fa42e6d9a29649943efc984ba4a92ab22d5e7d
Majo <ospinamj707@gmail.com>
2023-05-08 15:31:26 -0500

    Creación de components: Asignar viaje - Historial

---
+++ LINDS/src/app/LOGISTICO/asignacion-viaje/mostrar-viaje/mostrar-viaje.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-viaje',
+  templateUrl: './mostrar-viaje.component.html',
+  styleUrls: ['./mostrar-viaje.component.scss'],
+})
+export class MostrarViajeComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ LINDS/src/app/LOGISTICO/historial-viaje/mostrar-historial/mostrar-historial.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-historial',
+  templateUrl: './mostrar-historial.component.html',
+  styleUrls: ['./mostrar-historial.component.scss'],
+})
+export class MostrarHistorialComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
