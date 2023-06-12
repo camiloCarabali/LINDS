a3b4d6aa69dce9edcbcb9f67c261fcd6451e421b
camiloCarabali <73615948+camiloCarabali@users.noreply.github.com>
2023-06-11 22:16:19 -0500

    url

--- LINDS/src/services/shared.service.ts
+++ LINDS/src/services/shared.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class SharedService {
-  readonly APIURL = 'http://127.0.0.1:8000/';
+  readonly APIURL = 'http://18.118.155.123:8000/';
 
   constructor(private http: HttpClient) {}
 
