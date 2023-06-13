b956cf2f99d00446b7513f289ec00efd6f011a17
camiloCarabali <73615948+camiloCarabali@users.noreply.github.com>
2023-06-13 09:14:07 -0500

    Update settings.py

--- LindsDjangoAPI/LindsDjangoAPI/settings.py
+++ LindsDjangoAPI/LindsDjangoAPI/settings.py
@@ -143,4 +143,4 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="soportelinds@gmail.com"
-EMAIL_HOST_PASSWORD="oiozfxkzdyxhelfi"
+EMAIL_HOST_PASSWORD="gnhqetuuzrentatj"
